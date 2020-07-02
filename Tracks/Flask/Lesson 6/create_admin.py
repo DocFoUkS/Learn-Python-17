0de88619e33b5b2a517be67df1d899a5c2ff019c
Matveev Evgeniy <doc.fouks@yandex.ru>
2020-07-02 22:13:18 +0300

    Flask lesson 6 user registration, flask-migrate and work with errors

---
+++ Tracks/Flask/Lesson 6/create_admin.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from getpass import getpass
+
+from webweatherapp import create_app
+from webweatherapp.db import db
+from aebweatherapp.users.models import User
+
+app = create_app()
+
+with app.app_context():
+    username = input(u'Введите имя пользователя для администратора: ')
+
+    if User.query.filter(User.username == username).count():
+        print(u'Уже существует такое имя пользователя')
+        sys.exit(0)
+
+    password = getpass(u'Введите пароль: ')
+    password2 = getpass(u'Повторите введеный пароль: ')
+    if not password == password2:
+        print(u'Пароли не совпадают')
+        sys.exit(0)
+
+    new_user = User(username=username, role='admin')
+    new_user.set_password(password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    print(u'Пользователь {user} создан с id {id}'.format(
+                                                    user=new_user.username,
+                                                    id=new_user.id))
---
+++ Tracks/Flask/Lesson 6/webweatherapp/config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from datetime import timedelta
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+WEATHER_TOWN = 'Москва'
+WEATHER_TOKEN = 'b6800983191bf9717a22195ae27631e3'
+YA_GEOCODER = '44c4a4c8-2d1d-4be4-ace6-19828829c378'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@127.0.0.1:5433/Learn_Python'
+
+SECRET_KEY = 'uriehsgiuhdkj23wkfljoIg82357'
+
+REMEMBER_COOKIE_DURATION = timedelta(days=30)
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
